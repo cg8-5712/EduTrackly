fd63268b8c525718bb447e07242a689878368d72
cg8 <5712.cg8@gmail.com>
2025-08-29 18:20:50 +0800

    refactor: simplify route registration process
    - Replace individual route registrations with a dynamic array
    - Use Promise.all to load routes asynchronously
    - Improve code maintainability and readability
    
    This refactor allows for easier addition or modification of routes in the future, as they are now managed in a single array structure.

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-// app.js
 import express from "express";
 import configureExpress from "./src/config/express.js";
 import config from "./src/config/config.js";
 
-import homeworkRoutes from "./src/routes/homework.js";
-import authRoutes from "./src/routes/auth.js";
-import classRoutes from "./src/routes/class.js";
-import studentRoutes from "./src/routes/student.js";
-import analysisRoutes from "./src/routes/analysis.js";
-import systemRoutes from "./src/routes/system.js";
+const routes = [
+  { path: 'homework', route: import('./src/routes/homework.js') },
+  { path: 'auth', route: import('./src/routes/auth.js') },
+  { path: 'class', route: import('./src/routes/class.js') },
+  { path: 'student', route: import('./src/routes/student.js') },
+  { path: 'analysis', route: import('./src/routes/analysis.js') },
+  { path: 'system', route: import('./src/routes/system.js') }
+];
 
 const app = express();
-
-// 配置中间件
 configureExpress(app);
 
-// 路由注册（加上前缀）
-app.use(config.route.prefix + "/homework", homeworkRoutes);
-app.use(config.route.prefix + "/auth", authRoutes);
-app.use(config.route.prefix + "/class", classRoutes);
-app.use(config.route.prefix + "/student", studentRoutes);
-app.use(config.route.prefix + "/analysis", analysisRoutes);
-app.use(config.route.prefix + "/system", systemRoutes);
+// 批量注册路由
+Promise.all(routes.map(r => r.route)).then(modules => {
+  routes.forEach((r, index) => {
+    app.use(`${config.route.prefix}/${r.path}`, modules[index].default);
+  });
+});
+
 export default app;
