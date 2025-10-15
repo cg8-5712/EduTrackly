f03c0e7c4a582a558f01ab189d663414b38bb5ef
Cg8 <5712.cg8@gmail.com>
2025-10-15 22:59:59 +0800

    feat: add JWT authentication middleware to system route

--- src/routes/system.js
+++ src/routes/system.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getSystemController } from "../controllers/systemController.js";
+import jwtRequire from "../middleware/jwt_require.js";
 
 const router = express.Router();
 
-router.get("/", getSystemController);
+router.get("/", jwtRequire, getSystemController);
 
 export default router;
